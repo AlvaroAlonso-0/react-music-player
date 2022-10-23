3c6065b153317f05a35a7c073eec452c54e96904
Alvaro Alonso <alonso.miguel.alvaro1@gmail.com>
2022-10-23 14:35:31 +0200

    Included login with Spotify api

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./sidebar.css";
 import SidebarButton from './sidebarButton';
 import { MdFavorite, MdSpaceDashboard } from "react-icons/md";
 import { FaPlay, FaSignOutAlt } from "react-icons/fa";
 import { IoLibrary } from "react-icons/io5";
 import { ImFire } from "react-icons/im";
+import apiClient from '../../spotify';
 
 export default function Sidebar() {
+    const [image, setImage] = useState("https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png");
+    useEffect(() => {
+        apiClient.get("/me").then((res) => {
+            setImage(res.data.images[0].url);
+        });
+    }, []);
+
     return (
         <div className="sidebar-container">
             <img 
-                src="https://www.freepnglogos.com/uploads/one-piece-logo-18.png"
+                src={image}
                 className="profile-img" 
                 alt="profile"
             />
